84c31ac905305c60fcce859099f7e14166bc5088
hiro1103 <96195951+hiro1103@users.noreply.github.com>
2023-12-26 22:17:09 +0900

    chatModels

--- .vscode/test.py
+++
@@ -1,8 +0,0 @@
-from langchain.llms import OpenAI
-import os
-
-os.environ["OPENAI_API_KEY"] = "あなたのAPI_KEY"
-llm = OpenAI(model_name="text-davinci-003", temperature=0)
-
-result = llm("自己紹介してください。")
-print(result)
