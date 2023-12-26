96bda28cd9adb9fa6fb6836b4fc6f42d48733e10
hiro1103 <96195951+hiro1103@users.noreply.github.com>
2023-12-26 21:00:26 +0900

    Language models

---
+++ .vscode/test.py
@@ -0,0 +1,8 @@
+from langchain.llms import OpenAI
+import os
+
+os.environ["OPENAI_API_KEY"] = "あなたのAPI_KEY"
+llm = OpenAI(model_name="text-davinci-003", temperature=0)
+
+result = llm("自己紹介してください。")
+print(result)
