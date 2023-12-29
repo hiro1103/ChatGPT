ca7b7da4d35898efa9e31c2ad41f4d237e7f991c
hiro1103 <96195951+hiro1103@users.noreply.github.com>
2023-12-29 20:31:02 +0900

    出力結果

--- outPutParser.py
+++ outPutParser.py
@@ -1,6 +1,9 @@
 from pydantic import BaseModel, Field
 from langchain.output_parsers import PydanticOutputParser
 from langchain.prompts import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage
+import os
 
 
 # PydanticOutputParserを使ったPythonオブジェクトの取得
@@ -27,3 +30,11 @@ prompt = PromptTemplate(
 )
 formatters_prompt = prompt.format(dish="カレー")
 print(formatters_prompt)
+os.environ["OPENAI_API_KEY"] = "OPENAI_API_KEY"
+chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+messages = [HumanMessage(content=formatters_prompt)]
+output = chat(messages)
+print(output.content)
+
+recipe = parser.parse(output.content)
+print(recipe)
