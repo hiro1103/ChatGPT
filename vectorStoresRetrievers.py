244835f332dfb81b3dec31eb8d9ff8e47b69085e
hiro1103 <96195951+hiro1103@users.noreply.github.com>
2024-01-05 03:58:09 +0900

    1/5

---
+++ vectorStoresRetrievers.py
@@ -0,0 +1,49 @@
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.document_loaders import GitLoader
+import os
+
+
+# textをベクトル化、vector storesを準備して、ドキュメントをベクトル化
+# LangChainにおいて、テキストに関連するドキュメントを得るインターフェースをRetrievers
+
+
+def file_filter(file_path):
+    return file_path.endswith(".mdx")
+
+
+loader = GitLoader(
+    clone_url="https://github.com/langchain-ai/langchain",
+    repo_path="./langchain",
+    branch="master",
+    file_filter=file_filter,
+)
+
+raw_docs = loader.load()
+print(len(raw_docs))
+
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+
+docs = text_splitter.split_documents(raw_docs)
+print(len(docs))
+
+os.environ["OPENAI_API_KEY"] = "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"
+embeddings = OpenAIEmbeddings()
+
+query = "AWSのS3からデータを読み込むためのDocumentLoaderはありますか？"
+
+vector = embeddings.embed_query(query)
+print(len(vector))
+print(vector)
+
+db = Chroma.from_documents(docs, embeddings)
+retriever = db.as_retriever()
+query = "AWSのS3からデータを読み込むためのDocumentLoaderはありますか？"
+
+context_docs = retriever.get_relevant_documents(query)
+print(f"len = {len(context_docs)}")
+
+first_doc = context_docs[0]
+print(f"metadata = {first_doc.metadata}")
+print(first_doc.page_content)
